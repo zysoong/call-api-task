ea3bc1c7778ad8bbaa3e6a8ffd35220fe74b587a
Ziyang Song <ziyang.song@gmx.de>
2023-06-02 11:27:33 +0200

    Initial commit

---
+++ src/main/java/com/example/callapitask/CallApiTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.callapitask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallApiTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallApiTaskApplication.class, args);
+	}
+
+}
